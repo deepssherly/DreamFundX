46c4b9688754be9703037df2ddc2a46537f2a07a
santhoshunt <santhoshkumar2012002@gmail.com>
2021-04-17 23:23:25 +0530

    fixed somemore bugs

--- src/components/DetailsNavbar.js
+++ src/components/DetailsNavbar.js
@@ -28,8 +28,6 @@ function DetailsNavbar() {
                 Back
               </Link>
             </li>
-          
-       
           </ul>
         </div>
       </nav>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import '../css/normalize.css';
 import { Button } from '../components/Button';
 import { Link } from 'react-router-dom';
 import HomeNavbar from '../components/HomeNavbar';
+import Footer from "./Wrapper";
 
 function Home() {
   return (
@@ -40,22 +41,22 @@ function Home() {
           <img src="images/bg1.svg" className="svg-img" alt="bg1" />
           <img src="images/pattern2.svg" className="svg-img" alt="pattern2" />
         </div>
-        <div className="row">
-          <div className="col span-1-of-3 box">
+        <div className="row-new">
+          <div className="col-new span-1-of-3 box-new">
             <h3>
-              <i className="fas fa-book-reader box-icon"></i>Our Purpose
+              <i className="fas fa-book-reader box-new-icon"></i>Our Purpose
               </h3>
             <p>To unlock Human potential for Good</p>
           </div>
-          <div className="col span-1-of-3 box">
+          <div className="col-new span-1-of-3 box-new">
             <h3>
-              <i className="fas fa-bullseye box-icon"></i>Our Mission
+              <i className="fas fa-bullseye box-new-icon"></i>Our Mission
               </h3>
             <p>To provide greater accountability in crowd funding using blockchain-enabled Smart Contracts</p>
           </div>
-          <div className="col span-1-of-3 box">
+          <div className="col-new span-1-of-3 box-new">
             <h3>
-              <i className="fab fa-angellist box-icon"></i>Our Vision
+              <i className="fab fa-angellist box-new-icon"></i>Our Vision
               </h3>
             <p>To be the first choice of Micro Enterprises growth in India</p>
           </div>
@@ -65,7 +66,51 @@ function Home() {
           <img src="images/bg1.svg" className="svg-img" alt="bg1" />
         </div>
       </div>
-
+      <section className="section-2">
+        <div className="col-new span-1-of-7 col-new-box-1">
+            <img src="images/images2/number-line-orange2.png" className="number-line" alt="bg1"/>
+        </div>
+        <div className="col-new span-2-of-7 col-new-box-2">
+            <div className="box-new-1">
+                <div>
+                    <h3 className="col-new">Register</h3>
+                </div>
+                <hr/>
+                <hr/>
+                <hr/>
+                <p>start by signing up for an account here.<br></br>
+                    All you need is to sign up,<br></br>
+                    upload your documents and you are ready to start!</p>
+            </div>
+            <div className="box-new-2">
+                <h3>Start Investing</h3>
+                <p>Once your account is approved,<br></br>
+                you can start investing!<br></br>
+                Simply Log in to your dashboard<br></br>
+                to see the available campaigns
+                </p>
+            </div>
+            <div className="box-new-2">
+                <h3>Monitor Your Investment</h3>
+                <p>You can easily monitor investments via your dashboard<br></br>
+                    This is also where you can do that funds transfer in or out of your account</p>
+            </div>
+            <div className="apply-btn">
+                <a href="getstarted">Apply now!</a> 
+            </div>
+        </div>
+        <div className="col-new span-1-of-7 vl"></div>
+        <div className="col-new span-3-of-7 col-new-box-3">
+            <img src="images/images2/Numberline2 (2).png" className="tree" alt="bg1"/>
+            {/* <div className="box-1 bottom-box">
+                <h3>Need to speak with someone?</h3>
+                <br></br>
+                <p><i className="fas fa-phone-alt "></i> call us on xxxxxxx</p>
+                <br></br>
+                <p><i className="fas fa-mail-bulk"></i> Or email here</p>
+            </div> */}
+        </div>
+    </section>
 
     </>
   );
